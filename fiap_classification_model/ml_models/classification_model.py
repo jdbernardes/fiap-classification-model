9775c761514a7936ca781a50bfaa32fde04ab980
jdbernardes <juliocd.bernardes@gmail.com>
2024-07-29 20:21:54 -0300

    (classification_model.py) - removing common methods added to utils

--- fiap_classification_model/ml_models/classification_model.py
+++ fiap_classification_model/ml_models/classification_model.py
@@ -1,15 +1,18 @@
-import numpy as nd
-from typing import List
+import numpy as np
 
-# pre processing imports
-from sklearn.preprocessing import normalize
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split
+# Metrics processing imports
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
     recall_score,
     mean_absolute_percentage_error,
+)
+
+# Utils
+from fiap_classification_model.utils.ml_utils import (
+    normalize_data,
+    encode_labels,
+    splitter,
 )
 
 
@@ -19,38 +22,29 @@ class ClassificationModel:
         self._y = y
         self._SEED = 42
 
-    def normalize(self) -> nd.ndarray:
-        X = normalize(self._X)
-        return X
-
-    def categorize(self) -> nd.ndarray:
-        l_encoder = LabelEncoder()
-        y = l_encoder.fit_transform(self._y)
-        return y
-
-    def regular_spliter(self, percent: float = 0.30) -> List[nd.ndarray]:
-        X = self.normalize()
-        y = self.categorize()
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=percent, random_state=self._SEED
-        )
-        return X_train, X_test, y_train, y_test
-
-    def calculate_results(self, y_pred: nd.ndarray, y_test: nd.ndarray) -> list:
-        result: list = []
-        result.append(round(accuracy_score(y_pred, y_test), 4))
-        result.append(round(precision_score(y_pred, y_test), 4))
-        result.append(round(recall_score(y_pred, y_test), 4))
-        result.append(round(mean_absolute_percentage_error(y_pred, y_test), 4))
-        return result
-
     def run_model(self, model: object, test_split: float = 0.30) -> list:
         """
         This is the simple run method so it exptects the model
         object already with all relevant hyperarameters
         """
-        X_train, X_test, y_train, y_test = self.regular_spliter(test_split)
+        X = normalize_data(self._X)
+        y = encode_labels(self._y)
+        X_train, X_test, y_train, y_test = splitter(
+            features=X, classes=y, test_size=0.3
+        )
         model.fit(X_train, y_train)
         y_pred = model.predict(X_test)
-        result_list = self.calculate_results(y_pred, y_test)
+        result_list = self.calculate_results(y_pred, y_test, model)
         return result_list
+
+    def calculate_results(
+        self, y_pred: np.ndarray, y_test: np.ndarray, model: object
+    ) -> list:
+        result: list = []
+        result.append(model.__str__())
+        result.append(str(model.__class__()).replace("()", ""))
+        result.append(round(accuracy_score(y_pred, y_test), 4))
+        result.append(round(precision_score(y_pred, y_test), 4))
+        result.append(round(recall_score(y_pred, y_test), 4))
+        result.append(round(mean_absolute_percentage_error(y_pred, y_test), 4))
+        return result
